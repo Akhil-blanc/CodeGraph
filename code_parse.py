bec00a93c1183762fc010246c46344ac072b845c
Akhil-blanc <akhil0012893@gmail.com>
2024-12-24 16:46:26 +0530

    Initial experiment on map-reduce

---
+++ code_parse.py
@@ -0,0 +1,80 @@
+from git import Repo
+from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.parsers import LanguageParser
+from langchain_text_splitters import Language
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_chroma import Chroma
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+import os
+os.environ["GOOGLE_API_KEY"]=''
+
+
+loader = GenericLoader.from_filesystem(
+    r"D:\my_study\MAS_game",
+    glob="**/*",
+    suffixes=[".py"],
+    exclude=["**/non-utf8-encoding.py"],
+    parser=LanguageParser(language=Language.PYTHON, parser_threshold=500),
+)
+documents = loader.load()
+print(len(documents))
+
+for doc in documents:
+    print(doc.metadata)
+    if doc.metadata.get('content_type',None)=='simplified_code':
+        print(doc)
+
+
+python_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON, chunk_size=2000, chunk_overlap=200
+)
+texts = python_splitter.split_documents(documents)
+print(len(texts))
+
+db = Chroma.from_documents(texts, GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
+retriever = db.as_retriever(
+    search_type="mmr",  # Also test "similarity"
+    search_kwargs={"k": 8},
+)
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("placeholder", "{chat_history}"),
+        ("user", "{input}"),
+        (
+            "user",
+            "Given the above conversation, generate a search query to look up to get information relevant to the conversation",
+        ),
+    ]
+)
+
+retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user's questions based on the below context:\n\n{context}",
+        ),
+        ("placeholder", "{chat_history}"),
+        ("user", "{input}"),
+    ]
+)
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+qa = create_retrieval_chain(retriever_chain, document_chain)
+
+question = "list all the names of functions and classes"
+result = qa.invoke({"input": question})
+print(result["answer"])
